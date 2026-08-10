50f7ae2ab031c740850322558ddd06ce71c5a914
Nicolas Andreis <jnandreis@outlook.com>
2026-08-10 15:03:36 -0300

    add: header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Header } from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <Header/>
+      </main>
+    </>
+  )
+}
+
+export { App }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/logo/logo.png'
+import light from '../../assets/icons/light.svg'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+          <div>
+            <img src={logo} alt="logo" />
+            <h1>Character Counter</h1>
+          </div>
+          <button>
+            <img src={light} alt="luz" />
+          </button>
+        </header>
+    </div>
+  )
+}
+
+export  {Header}
